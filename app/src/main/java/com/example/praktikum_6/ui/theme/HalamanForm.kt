f4a320e3053af85442977ba63e38d376fbd5d5fe
Furjii <furjimadi@gmail.com>
2023-11-16 00:54:26 +0700

    menambahkan Column textfield

--- app/src/main/java/com/example/praktikum_6/ui/theme/HalamanForm.kt
+++ app/src/main/java/com/example/praktikum_6/ui/theme/HalamanForm.kt
@@ -1,16 +1,30 @@
 package com.example.praktikum_6.ui.theme
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
 
 @Composable
-fun HalamanForm(){
+fun HalamanForm(
+) {
+    var namaTxt by remember { mutableStateOf("") }
+    var alamatTxt by remember { mutableStateOf("") }
+    var tlpnTxt by remember { mutableStateOf("") }
 
-    var namaTxt by remember { mutableStateOf("")}
-    var alamatTxt by remember { mutableStateOf("")}
-    var tlpnTxt by remember { mutableStateOf("")}
+    var listData: MutableList<String> = mutableListOf(namaTxt, alamatTxt, tlpnTxt)
 
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+    }
 }
