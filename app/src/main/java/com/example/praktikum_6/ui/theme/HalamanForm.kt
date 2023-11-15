d85452012129eac81199c9b51867151f267ff382
Furjii <furjimadi@gmail.com>
2023-11-16 00:32:43 +0700

    menambahkan variabel nama,alamt,notelp

--- app/src/main/java/com/example/praktikum_6/ui/theme/HalamanForm.kt
+++ app/src/main/java/com/example/praktikum_6/ui/theme/HalamanForm.kt
@@ -1,8 +1,16 @@
 package com.example.praktikum_6.ui.theme
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 
 @Composable
 fun HalamanForm(){
+
+    var namaTxt by remember { mutableStateOf("")}
+    var alamatTxt by remember { mutableStateOf("")}
+    var tlpnTxt by remember { mutableStateOf("")}
 
 }
