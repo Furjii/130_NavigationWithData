be1ba1bb409a7f183babfc05ad26be1ceb3b3d13
Furjii <furjimadi@gmail.com>
2023-11-16 01:02:54 +0700

    menambahkan spacer button navigasi dan text pada button

--- app/src/main/java/com/example/praktikum_6/ui/theme/HalamanForm.kt
+++ app/src/main/java/com/example/praktikum_6/ui/theme/HalamanForm.kt
@@ -2,7 +2,10 @@ package com.example.praktikum_6.ui.theme
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
@@ -14,6 +17,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.praktikum_6.R
 
 
@@ -42,5 +46,11 @@ fun HalamanForm(
         OutlinedTextField(value = alamatTxt, onValueChange = { alamatTxt = it }, label = {
             Text(text = stringResource(id = R.string.alamat))
         })
+
+        Spacer(modifier = Modifier.height(15.dp))
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = stringResource(id = R.string.confirm))
+            
+        }
     }
 }
