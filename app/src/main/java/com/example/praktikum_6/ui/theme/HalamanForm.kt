31f5551282a3aab54f6c08c408eef6cfb5bd1602
Furjii <furjimadi@gmail.com>
2023-11-16 02:54:07 +0700

    revisi navigasi dan beberapa code and FINISH

--- app/src/main/java/com/example/praktikum_6/ui/theme/HalamanForm.kt
+++ app/src/main/java/com/example/praktikum_6/ui/theme/HalamanForm.kt
@@ -2,11 +2,14 @@ package com.example.praktikum_6.ui.theme
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,41 +20,77 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.praktikum_6.R
-
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HalamanForm(
-    onSubmitButtonClicked:(MutableList<String>) -> Unit
-) {
-    var namaTxt by remember { mutableStateOf("") }
-    var alamatTxt by remember { mutableStateOf("") }
-    var tlpnTxt by remember { mutableStateOf("") }
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    onBackButtonCLicked: () -> Unit
+){
 
-    var listData: MutableList<String> = mutableListOf(namaTxt, alamatTxt, tlpnTxt)
+    var namaText by remember {
+        mutableStateOf("")
+    }
+    var alamatText by remember {
+        mutableStateOf("")
+    }
+    var phoneText by remember {
+        mutableStateOf("")
+    }
+
+    var listData: MutableList<String> = mutableListOf(namaText, alamatText, phoneText)
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        OutlinedTextField(value = namaTxt, onValueChange = { namaTxt = it }, label = {
-            Text(text = stringResource(id = R.string.nama_p))
-        })
-        OutlinedTextField(value = tlpnTxt, onValueChange = { tlpnTxt = it }, label = {
-            Text(text = stringResource(id = R.string.nomor_p))
+        Text(text = "Masukan data diri", fontWeight = FontWeight.Bold, fontSize = 25.sp)
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = namaText,
+                shape = MaterialTheme.shapes.large,
+                label = { Text(text = stringResource(id = R.string.nameUser)) },
+                onValueChange = {namaText = it}
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
 
-        })
-        OutlinedTextField(value = alamatTxt, onValueChange = { alamatTxt = it }, label = {
-            Text(text = stringResource(id = R.string.alamat))
-        })
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = alamatText,
+                shape = MaterialTheme.shapes.large,
+                label = { Text(text = stringResource(id = R.string.addressUser)) },
+                onValueChange = {alamatText = it}
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
 
-        Spacer(modifier = Modifier.height(15.dp))
-        Button(onClick = { /*TODO*/ }) {
-            Text(text = stringResource(id = R.string.confirm))
-            
+            OutlinedTextField(
+                modifier = Modifier.fillMaxWidth(),
+                value = phoneText,
+                shape = MaterialTheme.shapes.large,
+                label = { Text(text = stringResource(id = R.string.phoneUser)) },
+                onValueChange = {phoneText = it}
+            )
+
+
+            Spacer(modifier = Modifier.padding(10.dp))
+        }
+        Row (modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ){
+            Button(onClick = {onBackButtonCLicked}) {
+                Text(text = stringResource(id = R.string.back_button))
+            }
+            Button(onClick = {onSubmitButtonClicked(listData)}) {
+                Text(text = stringResource(id = R.string.submit_button))
+            }
         }
     }
 }
